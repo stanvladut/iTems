2c2ee67e60692a9842af5e180a828facc56f30f8
Stan Vladut Angel <stanvladut@gmail.com>
2015-05-21 15:15:45 +0300

    updated remove button on cart updated place command button on cart

--- src/js/components/cart.js
+++ src/js/components/cart.js
@@ -11,20 +11,6 @@ var DesktopMmenu = require('../components/desktop-menu');
 var Slideshow = require('../components/slideshow');
 var ProductList = require('../components/product-list');
 
-var CartItem = React.createClass({
-    render: function(){
-        return (
-            <div className="pure-g cart-item">
-                <div className="pure-u-1-2 pure-u-sm-1-5"><img src={this.props.img} className="cart-image" /></div>
-                <div className="pure-u-1-2 pure-u-sm-1-5 cart-title">{this.props.titlu} lei</div>
-                <div className="pure-u-1-2 pure-u-sm-1-5 cart-price">500</div>
-                <div className="pure-u-1-2 pure-u-sm-1-5 cart-count">{this.props.nr_bucati} bucăți</div>
-                <div className="pure-u-1-2 pure-u-sm-1-5"><button>Remove</button></div>
-            </div>
-        );
-    }
-});
-
 var Cart = React.createClass({
     mixins: [ Router.State ],
     
@@ -57,26 +43,55 @@ var Cart = React.createClass({
             <div className="container">
                 <h1>Cosul de cumparaturi</h1>
                 {this.renderCart()}
-                <button className="cart-comanda">Comandă !</button>
+                <button className="cart-comanda" onClick={this.placeCommand}>Comandă !</button>
             </div>  
         );
   },
                              
-  renderCart: function()
-  {
-      if (this.state.array.length===0) 
-      {
+    renderCart: function()
+    {
+        if (this.state.array.length===0) 
+        {
           return <p>Cosul este gol</p>
-      }
-      else 
-      {
+        }
+        else 
+        {
           return this.state.array.map(this.renderProduct);
-      }
-  },
-      renderProduct: function(info)
+        }
+    },
+    renderProduct: function(info)
     {   
-        return <CartItem {...info} />
-    } 
+        var self=this;
+        return (
+            <div className="pure-g cart-item">
+                <div className="pure-u-1-2 pure-u-sm-1-5"><img src={info.img} className="cart-image" /></div>
+                <div className="pure-u-1-2 pure-u-sm-1-5 cart-title">{info.titlu} lei</div>
+                <div className="pure-u-1-2 pure-u-sm-1-5 cart-price">{info.pret}</div>
+                <div className="pure-u-1-2 pure-u-sm-1-5 cart-count">{info.nr_bucati} bucăți</div>
+                <div className="pure-u-1-2 pure-u-sm-1-5"><button onCLick={self.removeItem}>Remove</button></div>
+            </div>
+        );
+    },
+        
+    removeItem: function()
+    {
+        var self=this;
+        $.post('/', {type: "remove_item"})
+            .then(function(result) {
+            alert(result);
+            if (result!='failed') self.setState({array:result});
+        });
+    },
+        
+    placeCommand:function()
+    {
+        var self=this;
+        $.post('/', {type: "order"})
+            .then(function(status) {
+            alert(status);
+            if (status==='succes') self.setState({array:[]});
+        });
+    }
 });
 
 module.exports = Cart;
